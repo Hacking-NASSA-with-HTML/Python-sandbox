45da182790d15af539f31f94fbe46c12548db2b3
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-01-27 14:08:13 +0400

    feat: add array_manipulation basic methods

---
+++ Array_Basic_Manipulations/main.py
@@ -0,0 +1,14 @@
+# x = "Hello, Python"
+# print(x)
+
+# array is "slovar'" in python
+array = ["hello", 2, None, False, 3.0]
+
+print(array)
+# -> length of array
+print(len(array))
+print(array[0])
+# -> append
+array.append("addition-2")
+print(array)
+print(len(array))
